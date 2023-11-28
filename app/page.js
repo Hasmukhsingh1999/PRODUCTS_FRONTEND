4c5c280bebf2435cdb18a481d0f051adcb616995
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-28 12:02:54 +0530

    ...

--- app/page.js
+++ app/page.js
@@ -6,12 +6,12 @@ import React from "react";
 const page = () => {
   return (
     <div>
-           <section className="px-6 md:px-20 py-24">
+      <section className="px-6 md:px-20 py-24">
         <div className="flex max-xl:flex-col gap-16">
-          <div className="flex flex-col justify-center"> 
+          <div className="flex flex-col justify-center">
             <p className="small-text">
               Smart Shopping Starts Here:
-              <Image 
+              <Image
                 src="/assets/icons/arrow-right.svg"
                 alt="arrow-right"
                 width={16}
@@ -25,7 +25,8 @@ const page = () => {
             </h1>
 
             <p className="mt-6">
-              Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
+              Powerful, self-serve product and growth analytics to help you
+              convert, engage, and retain more.
             </p>
 
             <Searchbar />
@@ -38,10 +39,8 @@ const page = () => {
       <section className="trending-section">
         <h2 className="section-text">Trending</h2>
         <div className="flex flex-wrap gap-x-8 gap-y-16">
-          {['APlleI','Book','Sneckaet'].map((product)=>(
-            <div>
-              {product}
-            </div>
+          {["APlleI", "Book", "Sneckaet"].map((product) => (
+            <div>{product}</div>
           ))}
         </div>
       </section>
--- components/Searchbar.jsx
+++ components/Searchbar.jsx
@@ -1,16 +1,21 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 
 const Searchbar = () => {
-  const handleSubmit = () => {
-
-  };
+  const [searchPrompt, setSearchPrompt] = useState("");
+  const handleSubmit = (e) => {};
   return (
     <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
-        <input type="text" placeholder="Enter product link" className="searchbar-input"/>
-        <button type="submit" className="searchbar-btn">
-            Search
-        </button>
+      <input
+        type="text"
+        placeholder="Enter product link"
+        className="searchbar-input"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
+      />
+      <button type="submit" className="searchbar-btn">
+        Search
+      </button>
     </form>
   );
 };
