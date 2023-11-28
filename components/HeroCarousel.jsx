3f60cc7f9a310a93f8984f2da0fcb795992fe989
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-28 11:40:55 +0530

    commit

--- components/HeroCarousel.jsx
+++ components/HeroCarousel.jsx
@@ -1,23 +1,36 @@
-"use client"
+"use client";
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+import Image from "next/image";
+
+const heroImages = [
+  { imgUrl: "/assets/images/hero-1.svg", alt: "smartwatch" },
+  { imgUrl: "/assets/images/hero-2.svg", alt: "bag" },
+  { imgUrl: "/assets/images/hero-3.svg", alt: "lamp" },
+  { imgUrl: "/assets/images/hero-4.svg", alt: "air fryer" },
+  { imgUrl: "/assets/images/hero-5.svg", alt: "chair" },
+];
 
 const HeroCarousel = () => {
   return (
-    <Carousel>
-      <div>
-        <img src="assets/1.jpeg" />
-        <p className="legend">Legend 1</p>
-      </div>
-      <div>
-        <img src="assets/2.jpeg" />
-        <p className="legend">Legend 2</p>
-      </div>
-      <div>
-        <img src="assets/3.jpeg" />
-        <p className="legend">Legend 3</p>
-      </div>
+    <Carousel
+      showThumbs={false}
+      autoPlay
+      infiniteLoop
+      interval={2000}
+      showArrows={false}
+      showStatus={false}
+    >
+      {heroImages.map((image) => (
+        <Image
+          src={`image.imgUrl`}
+          alt={image.alt}
+          widht={484}
+          height={484}
+          key={image.alt}
+        />
+      ))}
     </Carousel>
   );
 };
