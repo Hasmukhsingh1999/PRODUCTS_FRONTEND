2ecdc5de079c31c655bd31b55d3fda2b88cebbe7
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-28 11:01:13 +0530

    added

--- app/page.js
+++ app/page.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <section className="px-6 border-2 border-red-500 md:px-20 py-24">
+        <div className="flex flex-col justify-center">
+          <p className="small-text">
+            Smart Shopping Starts Here:
+            <Image src="/assets/icons/arrow-right.svg" alt="arrow -right" width={16} height={16}/> 
+              
+            
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default page
+export default page;
