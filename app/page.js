c6d8fd2dd82b4ba590321a8b2726a0440e83adba
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-28 11:11:57 +0530

    commit

--- app/page.js
+++ app/page.js
@@ -3,18 +3,29 @@ import React from "react";
 
 const page = () => {
   return (
-    <>
+    <div>
       <section className="px-6 border-2 border-red-500 md:px-20 py-24">
         <div className="flex flex-col justify-center">
           <p className="small-text">
             Smart Shopping Starts Here:
-            <Image src="/assets/icons/arrow-right.svg" alt="arrow -right" width={16} height={16}/> 
-              
+            <Image
+              src="/assets/icons/arrow-right.svg"
+              alt="arrow -right"
+              width={16}
+              height={16}
+            />
+          </p>
+          <h1 className="head-text">
+            Unleash the Power of
+            <span className="text-primary">PriceWise</span>
+          </h1>
+          <p className="mt-6">
             
+
           </p>
         </div>
       </section>
-    </>
+    </div>
   );
 };
 
